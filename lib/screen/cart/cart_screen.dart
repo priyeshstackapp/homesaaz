3e64bb49ad03f4244097d70a6d041ab0d13f650a
Flutter app <brijeshbodariya@gmail.com>
2020-12-16 11:08:51 +0500

    Cart Screen continue button

--- lib/screen/cart/cart_screen.dart
+++ lib/screen/cart/cart_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:homesaaz/app.dart';
 import 'package:homesaaz/common/colorres.dart';
@@ -19,18 +20,47 @@ class CartScreenState extends State<CartScreen> {
     print("Current page --> $runtimeType");
     model ?? (model = CartViewModel(this));
 
+    Size media = MediaQuery.of(context).size;
+    double width = media.width;
+    double height = media.height;
+
     return Scaffold(
       appBar: commonAppbar(context),
       body: Stack(
+        alignment: Alignment.bottomCenter,
         children : [
           ListView.builder(
             itemCount: model.newProductName.length,
             itemBuilder: (context, index) {
               CartModel cartItem = model.newProductName[index];
             return cartProductView(cartItem,this,model.newProductName);
-          },)
+          },),
+          InkWell(
+            onTap: () {
+
+            },
+            child: Container(
+              alignment: Alignment.center,
+              height: height * 0.07,
+              width: width * 0.92,
+              margin: EdgeInsets.only(bottom: 30),
+              decoration: BoxDecoration(
+                color: ColorRes.red,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              child: Text(
+                'Continue',
+                style: new TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontFamily: 'NeueFrutigerWorld',
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
         ]
       ),
     );
   }
+
 }
