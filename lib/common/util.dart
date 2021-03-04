f83687bdd2b30af4bc450c46cab850aa10efa315
PS <priyeshstackapp@gmail.com>
2021-03-04 17:13:17 +0530

    Issue Fixed

--- lib/common/util.dart
+++ lib/common/util.dart
@@ -38,15 +38,3 @@ class Utils{
     return Injector.loginResponse != null;
   }
 }
-
-class  extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
